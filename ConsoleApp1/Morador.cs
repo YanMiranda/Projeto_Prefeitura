08966b848d1d5daf2479ca48560194f6b1de6c94
Yan Miranda <felipeyaann@gmail.com>
2022-05-14 13:20:47 -0300

    primeiro commit

---
+++ ConsoleApp1/Morador.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+
+
+
+namespace ConsoleApp1
+{
+    internal class Morador
+    {
+        static long CPF, TEL;
+        static double Renda;
+        static byte Dependentes;
+        static string Nome, Endereco;
+
+        public static void setCPF(long c)
+        {
+
+            if (c > 10000000000)
+                CPF = c;
+
+        }
+        public static long getCPF()
+        {
+            return CPF;
+        }
+        public static void setTEL(long t)
+        {
+
+            if (t > 10000000000)
+                TEL = t;
+
+        }
+        public static long getTEL()
+        {
+            return TEL;
+        }
+        public static void setRenda(double r)
+        {
+
+            if (r < 0)
+                Renda = r;
+
+        }
+        public static double getRenda()
+        {
+            return Renda;
+        }
+
+        public static void setDependentes(byte d)
+        {
+
+            if (d < 0)
+                Dependentes = d;
+
+        }
+        public static byte getDependentes()
+        {
+            return Dependentes;
+        }
+        public static void setEndereco(string e)
+        {
+
+            Endereco = e;
+
+        }
+        public static string getEndereco()
+        {
+            return Endereco;
+        }
+        public static void setNome(string n)
+        {
+
+            Nome = n;
+
+        }
+        public static string getNome()
+        {
+            return Nome;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void main(String[] args)
+        {
+
+            for (int i = 0; i <= 2; i++)
+            {
+                Morador MRD = new Morador();
+
+                Console.WriteLine("Qual o nome do morador:");
+                MRD.setNome() = Console.ReadLine();
+
+                Console.WriteLine("Qual o endereço do morador:");
+                MRD.endereço = Console.ReadLine();
+
+                Console.WriteLine("Qual o tel do morador:");
+                MRD.tel = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Qual o numero de dependentes do morador:");
+                MRD.dependententes = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Qual a renda do morador:");
+                MRD.renda = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Qual o CPF do morador:");
+                MRD.CPF = double.Parse(Console.ReadLine());
+
+                foreach (DictionaryEntry IMP in morador)
+
+
+            }
+
+
+        }
+
+    }
+}
